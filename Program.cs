33d749401f02bcfca81ed82464df9cd5d29deb99
Carlos Reyes <carlosrrg2002@gmail.com>
2023-03-04 12:22:45 -0600

    subinedo los archivo parcial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Ingrese un año: ");
+        int year = int.Parse(Console.ReadLine());
+        
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            Console.WriteLine("El año {0} es bisiesto.", year);
+        } else {
+            Console.WriteLine("El año {0} no es bisiesto.", year);
+        }
+    }
+}
+
